75a3bef41b428e1863cc18b689da76df708f651e
volantes <cvolantesilva@gmail.com>
2018-12-26 18:22:42 -0500

    blog tests

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -18,10 +18,99 @@ afterEach(async () => {
 test('When logged in, new blog form appears', async () => {
   await page.login();
 
-  //click on add blog button
+
   await page.click("a.btn-floating");
 
   const label = await page.getContentsOf('form label');
 
   expect(label).toEqual('Blog Title');
+});
+
+describe('When logged in', async () => {
+  beforeEach(async () => {
+    await page.login();
+
+    //click on add blog button
+    await page.click("a.btn-floating");
+  });
+
+  test('new blog form appears', async () => {
+    const label = await page.getContentsOf('form label');
+
+    expect(label).toEqual('Blog Title');
+  });
+
+  describe('And using invalid inputs', async () => {
+    beforeEach(async () => {
+      await page.click("form button");
+    });
+
+    test('the form shows and error message', async () => {
+      const titleError = await page.getContentsOf('.title .red-text');
+      const contentError = await page.getContentsOf('.content .red-text');
+
+      expect(titleError).toEqual('You must provide a value');
+      expect(contentError).toEqual('You must provide a value');
+    });
+  });
+
+  describe('And using valid inputs', async () => {
+    beforeEach(async () => {
+      await page.type(".title input", "My Title");
+      await page.type(".content input", "My Content");
+      await page.click("form button");
+    });
+
+    test('submitting takes user to review screen', async () => {
+      const title = await page.getContentsOf('h5');
+
+      expect(title).toEqual("Please confirm your entries");
+    });
+
+    test('submitting then saving takes user to /blogs', async () => {
+      await page.click('button.green');
+
+      await page.waitFor('.card');
+
+      const title = await page.getContentsOf('.card-title');
+      const content = await page.getContentsOf('p');
+
+      expect(title).toEqual('My Title');
+      expect(content).toEqual('My Content');
+    });
+  });
+});
+
+
+describe('When logged out', async () => {
+  test('can\'t create new blog posts', async () => {
+    const result = await page.evaluate(
+      () => {
+        return fetch('/api/blogs', {
+          method: 'POST',
+          credentials: 'same-origin',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({title: 'My Title', content: 'My Content'})
+        }).then(res => res.json());
+      });
+
+      expect(result).toEqual({ error: 'You must log in!' });
+  });
+
+  test('cannot retrieve blog posts', async () => {
+    const result = await page.evaluate(
+      () => {
+        return fetch('/api/blogs', {
+          method: 'GET',
+          credentials: 'same-origin',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }).then(res => res.json());
+      });
+
+      expect(result).toEqual({ error: 'You must log in!' });
+  })
 });
